9d9c36ef66b6875777dc0590a6a8386581667166
Sean Connolly <connolly.st@gmail.com>
2021-09-05 12:19:08 -1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# External module imports
+import time
+
+import RPi.GPIO as GPIO
+
+on = 60
+# off = 30
+
+relayPin = 18  # Broadcom pin 23 (P1 pin 16)
+
+GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
+GPIO.setup(relayPin, GPIO.OUT)  # LED pin set as output
+
+GPIO.output(relayPin, GPIO.HIGH)
+time.sleep(on)
+GPIO.output(relayPin, GPIO.LOW)
+# time.sleep(off)
